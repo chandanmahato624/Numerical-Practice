089bc0e5e8205239817e997816c1ba33da3435b5
chandan <chandanmahato624@gmail.com>
2021-03-16 01:16:32 +0530

    This code only for this math

---
+++ Bisection1stTest.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float a,b,x,s;
+    cout<<"The equation is : x^3-x-1=0"<<endl;
+    cout<<"Enter the value of A(-) : ";
+    cin>>a;
+    cout<<"Enter the value of B(+) : ";
+    cin>>b;
+    x = (a+b)/2;
+    s = (x*x*x)-x-1;
+
+    for(int i=0; i<11; i++){
+        if(s>0){
+            b=x;
+        }else{
+            a=x;
+        }
+         x = (a+b)/2;
+            s = (x*x*x)-x-1;
+    }
+    cout<<x;
+}
