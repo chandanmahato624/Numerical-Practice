344d273dd2f7608734755b56ad857870889637b4
chandan <chandanmahato624@gmail.com>
2021-03-16 16:16:11 +0530

    Correction with old Practical

--- ForwardIn.c
+++ ForwardIn.c
@@ -5,7 +5,7 @@ int main(){
     float x[100],y[100],z[100],a,h,u,point,result,k;
     printf("Enter the Interpolation size :");
     scanf("%d",&n);
-    for(i=1; i<=n; i++){
+    for(i=0; i<n; i++){
         printf("x[%d]=",i);
         scanf("%f",&x[i]);
         printf("y[%d]=",i);
@@ -14,21 +14,21 @@ int main(){
     printf("Enter the interpolation point :");
     scanf("%f",&point);
     //Forward interpolation:
-    a = x[1];
-    h = x[2]-x[1];
+    a = x[0];
+    h = x[1]-x[0];
     u = (point-a)/h;
-    for(i=1; i<=n; i++){
+    for(i=0; i<n; i++){
         z[i]=y[i];
     }
-    result = y[1];
+    result = y[0];
     k = 1;
     //Forward interpolation algoritham
-    for(i=2; i<=n; i++){
-        for(j=1; j<=n-i; j++){
+    for(i=1; i<n; i++){
+        for(j=0; j<n-i; j++){
             z[j]=z[j+1]-z[j];
         }
-        k *= (u-i+1)/i;
-        result += k*z[1];
+        k = (u-i+1)/i;
+        result += k*z[0];
     }
     printf("The result = %f",result);
 }
